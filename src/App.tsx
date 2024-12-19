54249ec6067207f26128e3f7c127fa7391692dde
Srikanthchow <kilarisrikanthchowdary@gmail.com>
2024-12-19 21:49:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './components/Login'
+
+export default function login() {
+  return (
+    <div>
+      <Login>
+        
+      </Login>
+    </div>
+  )
+}
